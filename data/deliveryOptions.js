9b2ffcf7bc178b0d715815eb291de928ad05947e
kingsley <kingsleykay26@gmail.com>
2025-09-16 11:46:46 +0100

    Save data and generate HTML for payment summary.

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,14 @@
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (deliveryOptionId === option.id) {
+      deliveryOption = option;
+    }
+  });
+  return deliveryOption || deliveryOptions[0];
+}
+
 export const deliveryOptions = [
   { id: "1", deliveryDays: 7, priceCents: 0 },
   { id: "2", deliveryDays: 3, priceCents: 499 },
---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,85 @@
+import { cart } from "../../data/cart.js";
+import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrency } from "../utils/money.js";
+import { getProduct } from "../../data/products.js";
+
+export function renderPaymentSummary() {
+  let ItemTotalPriceCents = 0;
+  let shippingTotalCents = 0;
+
+  cart.forEach((cartItem) => {
+    const productId = cartItem.productId;
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+    //the functions below gets the product and deliveryOption that matches with our product ID and deliveryOption ID.
+    const matchingProduct = getProduct(productId);
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
+
+    //console.log(deliveryOption);
+    //console.log(matchingProduct);
+
+    ItemTotalPriceCents += matchingProduct.priceCents * cartItem.quantity;
+
+    shippingTotalCents += deliveryOption.priceCents;
+  });
+
+  const totalBeforeTaxCents = ItemTotalPriceCents + shippingTotalCents;
+  const taxedTotalCents = totalBeforeTaxCents * 0.1;
+  const orderTotalCents = totalBeforeTaxCents + taxedTotalCents;
+
+  console.log(Math.round(ItemTotalPriceCents));
+  console.log(Math.round(shippingTotalCents));
+  console.log(totalBeforeTaxCents);
+  console.log(Math.round(taxedTotalCents));
+  console.log(Math.round(orderTotalCents));
+
+  const paymentSummaryHTML = `
+<div class="payment-summary-title">
+          Order Summary
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Items (3):</div>
+          <div class="payment-summary-money">
+            $${formatCurrency(ItemTotalPriceCents)}
+          </div>
+        </div>
+
+
+
+        <div class="payment-summary-row">
+          <div>Shipping &amp; handling:</div>
+          <div class="payment-summary-money">
+            $${formatCurrency(shippingTotalCents)}
+          </div>
+        </div>
+
+        <div class="payment-summary-row subtotal-row">
+          <div>Total before tax:</div>
+          <div class="payment-summary-money">
+            $${formatCurrency(totalBeforeTaxCents)}
+          </div>
+        </div>
+
+        <div class="payment-summary-row">
+          <div>Estimated tax (10%):</div>
+          <div class="payment-summary-money">
+            $${formatCurrency(taxedTotalCents)}
+          </div>
+        </div>
+
+        <div class="payment-summary-row total-row">
+          <div>Order total:</div>
+          <div class="payment-summary-money">
+            $${formatCurrency(orderTotalCents)}
+          </div>
+        </div>
+
+        <button class="place-order-button button-primary">
+          Place your order
+        </button>
+
+`;
+
+  document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
+}
